cfc9d31e32a19dc89438afdc55a696c1a11915f1
ancc02 <oandresccc@gmail.com>
2022-09-25 12:19:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config({ path: './.env' });
+
+const puerto = process.env.PORT || 3000;
+
+app.listen(puerto, () => {
+    console.log(`SERVER RUNNING IN PORT ${puerto}`);
+});
